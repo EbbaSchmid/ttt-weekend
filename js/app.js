35cf8c9b0afc5d23633421bf57480b1b5a1a4a58
EbbaSchmid <justebba@gmail.com>
2022-09-01 15:38:45 -0500

    step 2

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@
 
 
 
+
 /*----------------- Variables (state) ---------------*/
+
+// / Step 1 - Define the required variables used to track the state of the game
+
+let board, turn, winner
 
 
 
 /*--------- Cached Element References ------------------------*/
 
+// Step 2 - Store cached element references
 
+const squareEls = document.querySelectorAll(".board > div")
+console.log(squareEls)
+
+const messageEl = document.querySelector("#message")
+console.log(messageEl)
 
 /*-------------- Event Listeners -----------------------------*/
 
