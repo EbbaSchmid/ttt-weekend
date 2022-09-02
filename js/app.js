899f93c1e75d68f999d8b693eb20ba6685d18e7a
EbbaSchmid <justebba@gmail.com>
2022-09-02 09:51:42 -0500

    step 5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 /*---------------- Constants --------------------------*/
 
-
+let winningCombinations = [
+    [0, 1, 2],[3, 4, 5],[6, 7, 8],
+    [0, 3, 6],[1, 4, 7],[2, 5, 8],
+    [0, 4, 8],[2, 4, 6]
+];
 
 
 /*----------------- Variables (state) ---------------*/
@@ -43,7 +47,7 @@ function init(){
     turn = 1
 
  // 3e) Set the `winner` to `null`.
-    winner = 1
+    winner = null
 
  // 3f) Call a function called `render` at the end of the `init` function
     render()
